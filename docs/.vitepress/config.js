49e70553c5226b25e5dfead7b840b008a045b11d
aaaa0ggMC <60734273+aaaa0ggMC@users.noreply.github.com>
2025-01-30 22:51:24 +0800

    Update

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -11,12 +11,18 @@ export default {
   themeConfig:{
     nav:[
       {text:'学海无涯',link:'/keep_learning'},
+      {text:'长路漫漫',link:'/keep_exploring'},
       {text:'关于',link:'/about'},
     ],
     sidebar:{
       "/about":{
         text:"About Me",
         items:[
+          {text:"AboutMe",link:"/about#aboutme",items:[
+            {text:"个人介绍",link:"/about#self_intro",items:[
+              {text:"2025/1/30",link:"/about/#intro_2025_1_30"}
+            ]}
+          ]},
           {text:"高中生活",link:"/about/high_school"},
           {text:"学到的东西",link:"/about/skills"}
         ]
