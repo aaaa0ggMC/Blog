0de4d23859c6770163902ed0234199e2d9a62de0
aaaa0ggMC <60734273+aaaa0ggMC@users.noreply.github.com>
2025-01-31 19:36:55 +0800

    GitalkTest
    !!!

---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,13 @@
+import DefaultTheme from 'vitepress/theme-without-fonts';
+import './custom.css';
+import LayoutIndex from './layout/Index.vue'
+import type { Theme } from 'vitepress'
+
+import 'gitalk/dist/gitalk.css'
+
+const theme: Theme = {  
+  ...DefaultTheme,
+  Layout: LayoutIndex
+}
+
+export default DefaultTheme;
