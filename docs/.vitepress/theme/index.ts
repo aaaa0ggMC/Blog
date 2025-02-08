5da57dc32ffe787ea5eb8f4ea45ccfe8ce45583f
RockonDreaming <60734273+aaaa0ggMC@users.noreply.github.com>
2025-02-08 18:01:33 +0800

    可以了吗？？test5

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,7 +1,8 @@
 import DefaultTheme from 'vitepress/theme-without-fonts';
 import Layout from './layout/Layout.vue';
 import { useRoute } from 'vitepress';
-import { watch } from 'vue';
+import { watch, onMounted } from 'vue';
+import * as GPGModule from './layout/GPG';
 
 // 引入需要的外部资源
 // import './layout/GPG';  // 确保 GPG.ts 会被加载
@@ -18,31 +19,22 @@ export default {
   setup() {
     const route = useRoute();
     
+	onMounted(() => {
+		console.log('Vue mounted.')
+		console.log('GPG.ts 加载成功');
+		GPGModule.initGPG();
+    });
+	
     watch(
       () => route.path,
       () => {
         console.log("Route changed");
-        import('./layout/GPG')
-          .then((GPGModule) => {
-            console.log('GPG.ts 加载成功');
-            GPGModule.initGPG();
-          })
-          .catch((error) => {
-            console.error('加载 GPG.ts 时出错:', error);
-          });
+		console.log('GPG.ts 加载成功');
+		GPGModule.initGPG();
       }
     );
   },
   enhanceApp({ app }) {
-    if (typeof window !== 'undefined') {
-      import('./layout/GPG')
-        .then((GPGModule) => {
-          console.log('GPG.ts 加载成功');
-          GPGModule.initGPG();
-        })
-        .catch((error) => {
-          console.error('加载 GPG.ts 时出错:', error);
-        });
-    }
+    
   }
 };
